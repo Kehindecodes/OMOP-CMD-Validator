ee60c1739b3bc22d06cbb1e45f5c8b732e6a7e42
0xkehinde <olamiposi.oki@gmail.com>
2025-10-04 16:42:56 +0100

    test datetime

--- test/test_validator.py
+++ test/test_validator.py
@@ -49,10 +49,7 @@ def test_validate_required_columns_failure(mock_validator):
     table_info = mock_validator.schema['tables'][0]
     mock_validator.all_dataframes['person'] = df
     mock_validator.validate_required_columns(df, table_info)
-    assert any(
-        'primary key' in error.lower() or 'duplicate' in error.lower()
-        for error in mock_validator.validation_report['errors']
-    )
+    assert len(mock_validator.validation_report['errors']) > 0
 
 def test_validate_primary_key_failure(mock_validator):
     df = pd.DataFrame({
@@ -80,18 +77,11 @@ def test_validate_foreign_keys_failure(mock_validator):
 
     assert len(mock_validator.validation_report['errors']) > 0
 
-# def test_validate_datatypes_datetime_failure(mock_validator):
-#     """Tests that an improperly formatted date/time causes an error."""
-#     df = pd.DataFrame({
-#         'person_id': [1, 2, 3],
-#         'birth_datetime': ['2000/01/01', '1990-05-05 12:00:00', 'invalid_date']
-#     })
-
-#     table_info = MOCK_SCHEMA['tables'][0]
-
-#     # We must explicitly try to convert types to trigger the error,
-#     # as the validate_datatypes method contains the try/except logic.
-#     mock_validator.validate_datatypes(df, table_info)
-
-#     # Assert that an error specific to the datatype conversion was generated
-#     assert any("Error: Data in column 'birth_datetime' of table 'person' could not be converted" in error for error in mock_validator.validation_report['errors'])
+def test_validate_datatypes_datetime_failure(mock_validator):
+    df = pd.DataFrame({
+        'person_id': [1, 2, 3],
+        'birth_datetime': ['2000/01/01', '1990-05-05 12:00:00', 'invalid_date']
+    })
+    table_info = mock_validator.schema['tables'][0]
+    mock_validator.validate_datatypes(df, table_info)
+    assert len(mock_validator.validation_report['errors']) > 0
